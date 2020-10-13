b6c693610ee2a173e4ad98c33e0838dcfff1431b
edson.costa <edson.costa@mjv.com.br>
2020-10-13 10:39:22 -0300

    Meu primeiro commit.

---
+++ AulaPOO/src/classes/ClienteMySqlImpl.java
@@ -0,0 +1,11 @@
+package classes;
+
+public class ClienteMySqlImpl implements ClienteDAO {
+
+	@Override
+	public Integer incluir(Empresa empresa) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ AulaPOO/src/main/Main.java
@@ -0,0 +1,65 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import classes.Empresa;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		
+		Empresa emp1 = new Empresa();
+		Empresa emp2 = new Empresa();
+		Empresa emp3 = new Empresa();
+		emp3.setNome("Empresa 3");
+		
+		Empresa emp4 = new Empresa();
+		Empresa emp5 = new Empresa();
+		
+		List<Empresa> empresas = new ArrayList<>();
+		
+		empresas.add(emp1);
+		empresas.add(emp2);
+		empresas.add(emp3);
+		empresas.add(emp4);
+		empresas.add(emp5);
+		
+		int count = 0;
+		
+		
+		for(Empresa emp : empresas) {
+			System.out.println(emp.getNome());
+		}
+		
+		
+		for(int aux = 0; aux < empresas.size(); aux++) {
+			Empresa emp = empresas.get(aux);
+			
+			if("Empresa 3".equals(emp.getNome())) {
+				empresas.add(aux+1, new Empresa());
+			}
+			
+		}
+		
+		
+		
+		while(empresas.size() < count) {
+			Empresa emp = empresas.get(count);
+			
+			if(!"Empresa 3".equals(emp.getNome())) {
+				count++;
+				continue;
+			}
+			
+			//1000 linhas de codigo.
+			count++;
+		}
+		
+		
+		//....
+		
+	}
+
+}
