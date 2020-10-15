9208dbca377534ffd55d9740b2c61b2cef971f3c
EdsonMJV <72771251+EdsonMJV@users.noreply.github.com>
2020-10-15 08:52:35 -0300

    Update Main.java
    Teste de conflito

--- AulaPOO/src/main/Main.java
+++ AulaPOO/src/main/Main.java
@@ -10,7 +10,7 @@ public class Main {
 	public static void main(String[] args) {
 
 		
-		Empresa emp1 = new Empresa();
+		Empresa empresa1 = new Empresa();
 		Empresa emp2 = new Empresa();
 		Empresa emp3 = new Empresa();
 		emp3.setNome("Empresa 3");
@@ -20,7 +20,7 @@ public class Main {
 		
 		List<Empresa> empresas = new ArrayList<>();
 		
-		empresas.add(emp1);
+		empresas.add(empresa1);
 		empresas.add(emp2);
 		empresas.add(emp3);
 		empresas.add(emp4);
@@ -29,7 +29,7 @@ public class Main {
 		int count = 0;
 		
 		for(Empresa emp : empresas) {
-			System.out.println("Teste");
+			System.out.println("Teste de conflito");
 		}
 		
 		
